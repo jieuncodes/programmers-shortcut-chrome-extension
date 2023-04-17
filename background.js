a1dbdd1f5c1409693f338c4bc97632319d88002b
youredith <youredith3@gmail.com>
2023-04-17 16:45:13 +0900

    feat: press "submit code" btn

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 chrome.commands.onCommand.addListener(async (command) => {
   console.log("Command received:", command);
-  if (command === "run_code") {
-    let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-    if (tabs.length) {
-      const url = tabs[0].url;
-      console.log("URL:", url);
-      if (url && url.startsWith("https://school.programmers.co.kr")) {
-        console.log("Executing content script");
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          files: ["content.js"],
-        });
-      }
+  let scriptToExecute = (command === "run_code") ? "content.js" : (command === "submit_code") ? "submit.js": null;
+
+  let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (tabs.length) {
+    const url = tabs[0].url;
+    if (url && url.startsWith("https://school.programmers.co.kr")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: [scriptToExecute],
+      });
     }
   }
 });
